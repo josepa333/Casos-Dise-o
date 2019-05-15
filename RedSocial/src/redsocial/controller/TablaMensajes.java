b636caa9ea2a1d45e4aecf397ba302b814901262
angeloortega <angeloale1@gmail.com>
2019-05-15 01:50:13 -0600

    SocialVIP

---
+++ RedSocial/src/redsocial/controller/TablaMensajes.java
@@ -0,0 +1,51 @@
+package redsocial.controller;
+
+import java.util.ArrayList;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import redsocial.model.MensajeVip;
+
+public class TablaMensajes {
+    
+    public void ver_tabla(JTable pTabla,ArrayList<MensajeVip> mensajes){
+        
+        pTabla.setDefaultRenderer(Object.class, new Render());
+        DefaultTableModel tablaPredeterminada = new DefaultTableModel(){
+            public boolean isCellEditable(int row,int column){
+                return false;
+            }
+        };
+        tablaPredeterminada.addColumn("Fecha");
+        tablaPredeterminada.addColumn("Mensaje");
+        tablaPredeterminada.addColumn("Likes");
+        tablaPredeterminada.addColumn("Dislikes");
+        JButton botonLike = new JButton("Likes");
+        JButton botonDislike = new JButton("Dislike");
+        
+        Object fila[] = new Object[6];
+            
+        if(mensajes.size() > 0){
+            for(int i=0; i<mensajes.size(); i++){
+                fila[0] = mensajes.get(i).getFecha().toString();
+                fila[1] = mensajes.get(i).getContenido();
+                fila[2] = mensajes.get(i).getLikes();
+                fila[3] = mensajes.get(i).getDislikes();
+                fila[4] = botonLike;
+                fila[5] = botonDislike;
+                tablaPredeterminada.addRow(fila);
+            }
+        }
+        else{
+            fila[0] = "";
+            fila[1] = "";
+            fila[2] = "";
+            fila[3] = "";
+            fila[4] = botonLike;
+            fila[5] = botonDislike;
+            tablaPredeterminada.addRow(fila);
+        }
+        pTabla.setModel(tablaPredeterminada);
+        pTabla.setRowHeight(30);
+    }
+}
---
+++ RedSocial/src/redsocial/model/Seguidor.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package redsocial.model;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Angelo PC
+ */
+public class Seguidor extends IServidor{
+    private String idSeguidor;
+
+    public Seguidor(String idSeguidor) {
+        this.idSeguidor = idSeguidor;
+    }
+
+    public String getIdSeguidor() {
+        return idSeguidor;
+    }
+
+    public void setIdSeguidor(String idSeguidor) {
+        this.idSeguidor = idSeguidor;
+    }
+
+    @Override
+    public void procesarConexion() throws IOException, ClassNotFoundException{
+        Mensaje mensaje = leerMensaje();
+        
+        if(null == mensaje.getTipo()) {
+            System.out.println("No se reconocio el tipo de mensaje");
+        }
+        else switch (mensaje.getTipo()) {
+            case LIKEMENSAJE:
+                enviarMensaje( mensaje);
+                break;
+            case DISLIKEMENSAJE:
+                break;
+            case SEGUIRVIP:
+                break;
+            case DEJARSEGUIRVIP:
+                break;
+            default:
+                System.out.println("No se reconocio el tipo de mensaje");
+                break;
+        }
+    }
+}
