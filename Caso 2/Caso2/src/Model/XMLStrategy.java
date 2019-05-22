19c1c168db4bb4e42b7cc886967cf19b7b3f4a02
angeloortega <angeloale1@gmail.com>
2019-05-21 21:41:26 -0600

    eo

---
+++ Caso 2/Caso2/src/Model/XMLStrategy.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Angelo PC
+ */
+public class XMLStrategy implements IStrategy{
+
+    @Override
+    public void processText(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String readFile(String file) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
--- Caso 2/Caso2/src/Views/MainWindowController.java
+++ Caso 2/Caso2/src/Views/MainWindowController.java
@@ -5,6 +5,7 @@
  */
 package Views;
 
+import Model.FormatProvider;
 import Model.IStrategy;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,6 +13,15 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
 
 /**
  *
@@ -20,7 +30,8 @@ import java.awt.event.MouseListener;
 public class MainWindowController implements KeyListener, ActionListener, MouseListener{
 
     private MainWindow view;
-    private IStrategy textProcessor;
+    private FormatProvider formatProvider;
+    private String path; //FilePath currrently being handled
     
     public MainWindowController() {
         view = new MainWindow();
@@ -37,9 +48,36 @@ public class MainWindowController implements KeyListener, ActionListener, MouseL
         view.cutButton.addActionListener(this);
         view.pasteButton.addActionListener(this);
         view.TextArea.addMouseListener(this);
+        this.formatProvider = new FormatProvider();
+        this.path = "";
     }
-    
-    
+    public static String readFile(String path, Charset encoding) 
+    throws IOException 
+    {
+      byte[] encoded = Files.readAllBytes(Paths.get(path));
+      return new String(encoded, encoding);
+    }
+    private void openFile(){
+        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+            String str = "";
+            jfc.setDialogTitle("Select an image");
+            jfc.setAcceptAllFileFilterUsed(false);
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("txt,xml,xsv,json", "txt", "xml","csv", "json");
+            jfc.addChoosableFileFilter(filter);
+            int returnValue = jfc.showOpenDialog(null);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+            try {
+                this.path = jfc.getSelectedFile().getPath();
+                String[] strArray = path.split("\\.");
+                String extension =  strArray[strArray.length - 1];
+                formatProvider.setFormatStrategy(extension);
+                str = this.formatProvider.readFile(readFile(path,Charset.defaultCharset()));
+                view.TextArea.setText(str);
+            } catch (Exception ex) {
+                Logger.getLogger(MainWindowController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+		}
+    }
     @Override
     public void mousePressed(MouseEvent e) {
     }
@@ -83,7 +121,7 @@ public class MainWindowController implements KeyListener, ActionListener, MouseL
                System.out.println("New");
                break;
            case "Open":
-               System.out.println("Open");
+               openFile();
                break;
            case "Save":
                System.out.println("Save");
